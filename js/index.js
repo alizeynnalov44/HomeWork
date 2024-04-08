0fef30cfbc1abcc989349a072e20e800c3ddaf92
alizeynnalov14 <mi87TUNJY@code.edu.az>
2024-04-08 02:50:26 -0700

    commit message

---
+++ js/index.js
@@ -0,0 +1,95 @@
+
+//for (let index = 2; index <= 100; index=index+2) {
+   // console.log(index);
+    
+//}
+
+
+//let eded = parseInt(prompt("eded yazin"))
+
+//if(eded % 2 == 0) {
+  //  console.log("eded cutdur");
+//}
+//else if (eded % 2 != 0) {
+//    console.log("eded tekdir");
+//}
+
+
+/*let month = parseFloat(prompt("ay yazin"))
+switch (month) {
+    case January :
+        console.log("January has 31 days");
+        break;
+      
+    case February :
+        console.log("February has 28 days");
+        break;
+    case March :
+        console.log("March has 31 days");
+        break;
+      
+    case April :
+        console.log("April has 30 days");
+        break;
+       
+    case May :
+        console.log("May has 31 days");
+        break;
+        
+    case  June:
+        console.log("June has 30 days");
+        break;
+        
+    case July :
+        console.log("July has 31 days");
+        break;
+        
+    case August:
+        console.log("August has 31 days");
+        break;
+        
+    case  September:
+        console.log("September has 31 days");
+        break;
+        
+    case Octomber :
+        console.log("Octomber has 31 days");
+        break;
+        
+    case Novomber :
+        console.log("Novomber has 31 days");
+        
+        break;
+    case December :
+        console.log("December has 31 days");
+        
+        break;
+
+    default:*/
+        
+//}
+
+/*
+let a = parseInt(prompt("katet1 yaz"))
+let b = parseInt(prompt("katet2 yaz"))
+let c = parseInt(prompt("kate3 yaz"))
+let sum =(a+b)
+if (sum>c) {
+    console.log(`duzbucaqli ucbucaqdir`);
+} else {
+    console.log(`duzbucaqli ucbucaq deyil`);
+    
+}*/
+
+/*
+let a = parseInt(prompt("a terefi"))
+let b = parseInt(prompt("b terefi"))
+let c = parseInt(prompt("c terefi"))
+if (a===b,a===c,b===c) {
+    console.log(`beraberyanli ucbucaqdir`);
+} else { if (a!==b,a!==c,b!==c) {
+    console.log(`beraberyanli ucbucaq deyil`);
+}
+    
+}
+*/
